c9b86c8ca5fa069d7f67b506823cd1e1df342f1a
Nikita Muravskiy <nik.myr.2000@gmail.com>
2022-11-15 20:54:05 +0300

    new louncher setup

--- Louncher.py
+++ Louncher.py
@@ -1,33 +1,25 @@
-from tkinter import *
-from tkinter import ttk
+import pyglet
 import os
 
-# create louncher window
-Louncher = Tk(className='Louncher for my minecraft')
-
-def start_game():
-    Louncher.destroy
-    os.system('cd /Users/nikita/Dev/Python_projects/my_minecraft')
-    os.system('python3 main.py')
-
-
-# window size settings
-Louncher.geometry('800x600')
-Louncher.resizable(False, False)
-
-
-# walpapper image
-walpapper = PhotoImage(file='/Users/nikita/Dev/Python_projects/my_minecraft/walpapper.png')
-label_walpapper = Label(Louncher, image=walpapper)
-label_walpapper.place(x=0, y=0)
-
-start_button = ttk.Button(Louncher, text='Start game', command=lambda: start_game())
-start_button.place(x=350, y=200)
-
-quit_button = ttk.Button(Louncher, text='Quit', command=Louncher.destroy)
-quit_button.place(x=360, y=400)
+class Louncher_window(pyglet.window.Window):
+    def __init__(self, **args,):
+        super().__init__(**args)
+    
+    def on_draw(self):
+        self.clear()
+        walpapper = pyglet.sprite.Sprite(img = pyglet.image.load('walpapper.png'))
+        walpapper.draw()
 
 
 
+class Louncher:
+    def __init__(self) -> None:
+        self.window = Louncher_window(width = 800, height = 600, caption = 'Louncher for <My minecraft>', resizable = False, vsync = False)
+        
+    def run(self):
+        pyglet.app.run()
 
-Louncher.mainloop()
+
+if __name__ == '__main__':
+    louncher = Louncher()
+    louncher.run()
